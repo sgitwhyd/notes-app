3503277464f362cc8731f31877b60383ad4df3b7
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-07-30 16:57:19 +0700

    feat: adding session provider next-auth & root layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../styles/global-styles.css";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import RootLayout from "@/components/layouts/RootLayout";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <SessionProvider session={pageProps.session}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <RootLayout>
+            <Component {...pageProps} />
+          </RootLayout>
+        </QueryClientProvider>
+      </SessionProvider>
     </>
   );
 };
